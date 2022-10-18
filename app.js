e670304b061c19c5c7cd8044d6cdfb2d47cb3f23
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-18 14:45:43 +0300

    setting up the put and delete rewquets

--- app.js
+++ app.js
@@ -46,3 +46,35 @@ postButton.addEventListener('click', function () {
         fetchEmployees();
     });
 })
+
+// put request
+let putButton = document.querySelector('#put-btn');
+putButton.addEventListener('click', function () {
+    let empID = '_abcde',
+        let employee = {
+            id: empID,
+            first_name: 'john',
+            last_name: 'wislon',
+            email: 'johnwilson@gmail.com',
+            gender: 'male',
+            ip_address: '255.255.255.255'
+        }
+    let url = `${serverURL}/employee/${empID}`;
+    let http = new brainhttp();
+    http.put(url, employee, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees();
+    })
+})
+
+// delete buttom
+let deleteButton = document.querySelector('#delete-btn');
+deleteButton.addEventListener('click', function () {
+    let employeeID = `_vwxyz`,
+        let url = `${server}/employee`;
+    let http = new brainhttp();
+    http.delete(url, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees();
+    })
+})
